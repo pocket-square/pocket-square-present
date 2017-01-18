8e866cfe3be2bc820437dbb9252b88a8b5394170
Yaroslav Yermilov <yermilov17@gmail.com>
2017-01-18 10:47:58 +0200

    create service image

---
+++ Dockerfile
@@ -0,0 +1,3 @@
+FROM python:2-onbuild
+
+CMD [ "python", "pocket-square-present.py" ]
--- pocket-square-present.py
+++ pocket-square-present.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 @app.route('/index')
 def index():
-    request = Request('http://localhost:5001/sort')
+    request = Request('http://pocket_square_sort_shuffle:5001/sort')
     try:
         response_json = urlopen(request).read()
         response = json.loads(response_json)

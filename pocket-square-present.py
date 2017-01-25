49d46eb6f84520cfafe5ad95bf8fdf3b6d74efb4
Yaroslav Yermilov <yermilov17@gmail.com>
2017-01-25 02:29:29 +0200

    use user id

--- pocket-square-present.py
+++ pocket-square-present.py
@@ -5,9 +5,9 @@ import json
 app = Flask(__name__)
 
 
-@app.route('/index')
-def index():
-    request = Request('http://pocket_square_sort_shuffle:5001/sort')
+@app.route('/index/<user_id>')
+def index(user_id):
+    request = Request('http://pocket_square_sort_shuffle:28104/sort/' + user_id)
     try:
         response_json = urlopen(request).read()
         response = json.loads(response_json)

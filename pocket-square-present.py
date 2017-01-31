442a540940d180d84c0813aa82a1b33e04f77860
Yaroslav Yermilov <yermilov17@gmail.com>
2017-01-31 17:03:11 +0200

    show user name

--- pocket-square-present.py
+++ pocket-square-present.py
@@ -7,9 +7,13 @@ app = Flask(__name__)
 
 @app.route('/index/<user_id>')
 def index(user_id):
-    request = requests.get('http://pocket_square_sort_shuffle:5000/sort/' + user_id)
-    response = request.json()
-    return render_template('index.html', posts=response)
+    posts_request = requests.get('http://pocket_square_sort_shuffle:5000/sort/' + user_id)
+    posts = posts_request.json()
+
+    user_request = requests.get('http://pocket_square_users:8080/user/' + user_id)
+    user = user_request.json()
+    
+    return render_template('index.html', posts=posts, user=user)
 
 
 if __name__ == '__main__':

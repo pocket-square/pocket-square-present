601ef427abee02b34f48d31b3646475231ebe0fd
Vlad Grytsun <gritsun@Vlads-MacBook-Pro.local>
2017-03-02 01:25:48 -0800

    fixed endpoints in presenter

--- pocket-square-present.py
+++ pocket-square-present.py
@@ -7,14 +7,30 @@ app = Flask(__name__)
 
 @app.route('/index/<user_id>')
 def index(user_id):
-    posts_request = requests.get('http://pocket_square_sort_shuffle:5000/sort/' + user_id)
+    #posts_request = requests.get('http://pocket_square_sort_shuffle:5000/sort/' + user_id)
+    posts_request = requests.get('http://188.166.174.189:28104/sort/' + user_id)
     posts = posts_request.json()
 
-    user_request = requests.get('http://pocket_square_users:8080/user/' + user_id)
+    for post in posts:
+        post['similar_url'] = 'http://0.0.0.0:4001/index/%s/similar/%s' % (user_id, post['id'])
+
+    # user_request = requests.get('http://pocket_square_users:8080/user/' + user_id)
+    user_request = requests.get('http://188.166.174.189:28101/user/' + user_id)
     user = user_request.json()
     
     return render_template('index.html', posts=posts, user=user)
 
 
+@app.route('/index/<user_id>/similar/<text_id>')
+def similar(user_id, text_id):
+    # posts_request = requests.get('http://pocket_square_sort_shuffle:5000/sort/' + user_id)
+    posts_request = requests.get('http://0.0.0.0:5001/%s/%s/similar_by_text' % (user_id, text_id))
+    posts = posts_request.json()
+
+    # user_request = requests.get('http://pocket_square_users:8080/user/' + user_id)
+
+    return render_template('similar.html', posts=posts[1:], original_post=posts[0])
+
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', port=4001)

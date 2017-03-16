6424a839532681c6cff00b6d2bed218dd948894e
Yaroslav Yermilov <yermilov17@gmail.com>
2017-03-16 23:59:35 +0200

    fixed: underscores are not allowed in host names

--- pocket-square-present.py
+++ pocket-square-present.py
@@ -2,18 +2,20 @@ from flask import render_template, Flask
 import requests
 import json
 
+LOCALHOST = os.environ['LOCALHOST']
+
 app = Flask(__name__)
 
 
 @app.route('/index/<user_id>')
 def index(user_id):
-    posts_request = requests.get('http://pocket_square_sort_shuffle:5000/sort/' + user_id)
+    posts_request = requests.get('http://pocket-square-sort_shuffle:5000/sort/' + user_id)
     posts = posts_request.json()
 
     for post in posts:
-        post['similar_url'] = 'http://188.166.174.189:8080/index/%s/similar/%s' % (user_id, post['id'])
+        post['similar_url'] = '%s/index/%s/similar/%s' % (LOCALHOST, user_id, post['id'])
 
-    user_request = requests.get('http://pocket_square_users:8080/user/' + user_id)
+    user_request = requests.get('http://pocket-square-users:8080/user/' + user_id)
     user = user_request.json()
 
     return render_template('index.html', posts=posts, user=user)
@@ -21,7 +23,7 @@ def index(user_id):
 
 @app.route('/index/<user_id>/similar/<text_id>')
 def similar(user_id, text_id):
-    posts_request = requests.get('http://pocket_square_similar_by_text:5000/%s/%s/similar_by_text' % (user_id, text_id))
+    posts_request = requests.get('http://pocket-square-similar-by-text:5000/%s/%s/similar_by_text' % (user_id, text_id))
     posts = posts_request.json()
 
     return render_template('similar.html', posts=posts[1:], original_post=posts[0])

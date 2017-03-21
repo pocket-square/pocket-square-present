e1cbda922cd03f4213b0acf129677af0005c8d47
Vlad Grytsun <gritsun@users.noreply.github.com>
2017-03-20 22:49:17 -0700

    fix environment

--- pocket-square-present.py
+++ pocket-square-present.py
@@ -1,14 +1,14 @@
 from flask import render_template, Flask, request
 import requests
-import json
 import os
 import yaml
 
 with open("application.yml", 'r') as ymlfile:
     cfg = yaml.load(ymlfile)
 
-environment = os.environ['SERVICE_ENVIRONMENT']
-if not environment:
+if os.environ.has_key('SERVICE_ENVIRONMENT'):
+    environment = os.environ['SERVICE_ENVIRONMENT']
+else:
     environment = cfg['default_environment']
 
 app = Flask(__name__)
